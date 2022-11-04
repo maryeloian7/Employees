b6e052e421837ff7fa61bb32ca310a7206f33ecd
Maryia <maritsydik@gmail.com>
2022-11-04 15:48:16 +0100

    new project-react

---
+++ src/components/app/app.js
@@ -0,0 +1,35 @@
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+
+function App(){
+
+    const data = [
+        {name: 'John C.', salary: 1300, increase: false, id: 1},
+        {name: 'Mary E.', salary: 3000, increase: true , id: 2},
+        {name: 'Kiki P.', salary: 3700, increase: false, id: 3},
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployeesList data={data}/>
+            <EmployeesAddForm />
+           
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css'
+
+const SearchPanel = () => {
+    return (
+        <input 
+            type="text" 
+            className="from-control search-input"
+            placeholder="Find an employee"/>
+    )
+}
+
+export default SearchPanel;
